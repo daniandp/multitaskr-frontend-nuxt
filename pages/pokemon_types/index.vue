f0501779a99df18b585ed2076c6ff2cc20b3dd50
daniandp <andradegsp@gmail.com>
2022-11-04 14:28:10 -0500

    global pagination

--- pages/pokemon_types/index.vue
+++ pages/pokemon_types/index.vue
@@ -1,42 +1,10 @@
 <template>
     <b-container class="py-5">
         <h1>Pokemon's Type</h1>
-        <b-row align-h="end">
-            <b-col>
-                <b-form-input
-                    v-model="search"
-                    type="search"
-                    placeholder="Search pokemon's type..."
-                ></b-form-input>
-            </b-col>
-            <b-col cols="3">
-                <b-form-select
-                    v-model="form.limit"
-                    :options="options"
-                ></b-form-select>
-            </b-col>
-            <b-col cols="2" class="mb-3">
-                <b-button-group class="w-25">
-                    <b-button
-                        @click="onSearch(-1)"
-                        variant="info"
-                        :disabled="is_previous_disabled"
-                    >
-                        Previous
-                    </b-button>
-                    <b-button
-                        @click="onSearch()"
-                        variant="info"
-                        :disabled="is_next_disabled"
-                    >
-                        Next
-                    </b-button>
-                </b-button-group>
-            </b-col>
-        </b-row>
+        <Pagination :count="types.count" />
         <b-table class="text-center" :items="types.results" :fields="fields">
             <template #cell(index)="data">
-                {{ data.index + 1 + form.offset }}
+                {{ data.index + 1 }}
             </template>
             <template #cell(name)="data">
                 <nuxt-link :to="'/pokemon_types/' + (data.index + 1)">
@@ -49,44 +17,30 @@
 
 <script>
 import { mapGetters } from "vuex";
-import debounce from "lodash.debounce";
+import Pagination from "@/components/Pagination.vue";
 
 export default {
     async fetch({ store }) {
         await store.dispatch("types/get"); // Actions del store de type
     },
 
+    components: {
+        Pagination,
+    },
+
     data() {
         return {
-            options: [
-                { value: null, text: "Limit", disabled: true },
-                { value: 10, text: "10" },
-                { value: 20, text: "20" },
-            ],
-
-            search: "",
-            form: {
-                offset: 0,
-                limit: 10,
-                search: "",
-            },
-
             fields: ["index", { key: "name", label: "Pokemon's Type" }, "url"],
         };
     },
 
     watch: {
-        form: {
+        "$route.query": {
             deep: true,
             async handler() {
-                await this.$store.dispatch("types/get", this.form);
+                await this.$store.dispatch("types/get", this.$route.query);
             },
         },
-
-        search: debounce(async function () {
-            this.form.search = this.search;
-            console.log("funciona el debounce");
-        }, 2000),
     },
 
     computed: {
@@ -95,24 +49,6 @@ export default {
             // acceso a las variables globales
             types: "types/items",
         }),
-
-        is_previous_disabled() {
-            return this.form.offset <= 0;
-        },
-
-        is_next_disabled() {
-            return this.form.offset + this.form.limit >= this.types.count;
-        },
-    },
-
-    methods: {
-        onSearch(pagination = 1) {
-            if (pagination > 0) {
-                this.form.offset += this.form.limit;
-            } else {
-                this.form.offset -= this.form.limit;
-            }
-        },
     },
 };
 </script>
--- pages/pokemons/index.vue
+++ pages/pokemons/index.vue
@@ -1,42 +1,10 @@
 <template>
     <b-container class="py-5">
         <h1>Pokemons</h1>
-        <b-row class="mb-3" align-h="end">
-            <b-col>
-                <b-form-input
-                    v-model="search"
-                    type="search"
-                    placeholder="Search pokemon..."
-                ></b-form-input>
-            </b-col>
-            <b-col cols="3">
-                <b-form-select
-                    v-model="form.limit"
-                    :options="options"
-                ></b-form-select>
-            </b-col>
-            <b-col cols="2">
-                <b-button-group>
-                    <b-button
-                        @click="onSearch(-1)"
-                        :disabled="is_previous_disabled"
-                        variant="info"
-                    >
-                        Previous
-                    </b-button>
-                    <b-button
-                        @click="onSearch()"
-                        :disabled="is_next_disabled"
-                        variant="info"
-                    >
-                        Next
-                    </b-button>
-                </b-button-group>
-            </b-col>
-        </b-row>
+        <Pagination :count="pokemons.count"/>
         <b-table class="text-center" :items="pokemons.results" :fields="fields">
             <template #cell(index)="data">
-                {{ data.index + 1 + form.offset }}
+                {{ data.index + 1  }}
             </template>
             <template #cell(url)="data">
                 <nuxt-link :to="'/pokemons/' + (data.index + 1)">
@@ -49,71 +17,39 @@
 
 <script>
 import { mapGetters } from "vuex";
-import debounce from "lodash.debounce";
+import Pagination from "@/components/Pagination.vue";
 
 export default {
-    async fetch({ store }) {
-        await store.dispatch("pokemons/get"); // Actions
+    async fetch({ store, route }) {
+        await store.dispatch("pokemons/get", route.query); // Actions
+    },
+
+    components: {
+        Pagination,
+        //"pagination-element": Pagination
     },
 
     data() {
         return {
-            options: [
-                { value: null, text: "Limit" },
-                { value: 20, text: "20" },
-                { value: 40, text: "40" },
-                { value: 80, text: "80" },
-                { value: 160, text: "160" },
-            ],
-
-            search: "",
-            form: {
-                offset: 0,
-                limit: 20,
-                search: "",
-            },
-
             fields: ["index", { key: "name", label: "Pokemon's Name" }, "url"],
         };
     },
 
     watch: {
-        form: {
+        '$route.query': {
             deep: true,
             async handler() {
-                await this.$store.dispatch("pokemons/get", this.form);
+                await this.$store.dispatch("pokemons/get", this.$route.query);
                 console.log("funciona el handler");
             },
         },
-
-        search: debounce(async function () {
-            console.log("funciona el search");
-            this.form.search = this.search;
-        }, 2000),
     },
 
     computed: {
         ...mapGetters({
             pokemons: "pokemons/items",
         }),
-
-        is_previous_disabled() {
-            return this.form.offset <= 0;
-        },
-
-        is_next_disabled() {
-            return this.form.offset + this.form.limit >= this.pokemons.count;
-        },
     },
 
-    methods: {
-        onSearch(pagination = 1) {
-            if (pagination > 0) {
-                this.form.offset += this.form.limit;
-            } else {
-                this.form.offset -= this.form.limit;
-            }
-        },
-    },
 };
 </script>
