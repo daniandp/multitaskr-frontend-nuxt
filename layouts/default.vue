194c0beaa9a0c1bd17ce16d41f425882f937eefb
daniandp <andradegsp@gmail.com>
2022-11-04 14:38:52 -0500

    abilitys pagination

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,14 +5,9 @@
                 <b-navbar-brand to="/">Daniela's Project</b-navbar-brand>
 
                 <b-navbar-nav>
-                    <b-nav-item to="/pokemons">
-                        Pokemons
-                    </b-nav-item>
-                    <b-nav-item to="/pokemon_types">
-                        Types
-                    <b-nav-item to="/abilities">
-                        Abilitys
-                    </b-nav-item>
+                    <b-nav-item to="/pokemons"> Pokemons </b-nav-item>
+                    <b-nav-item to="/pokemon_types"> Types </b-nav-item>
+                    <b-nav-item to="/abilities"> Abilitys </b-nav-item>
                 </b-navbar-nav>
             </b-container>
         </b-navbar>
